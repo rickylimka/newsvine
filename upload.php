1a431c657b2cc2bc1839d5745acaee17f32a6548
Ricky Limka <rikki@chikurin.example.org>
2015-10-02 00:53:31 +1000

    added choose file form.

--- upload.php
+++ upload.php
@@ -2,9 +2,10 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+/* if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
@@ -13,9 +14,7 @@ if(isset($_POST["submit"])) {
         echo "File is not an image.";
         $uploadOk = 0;
     }
-}
-
-
+} */
 
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
