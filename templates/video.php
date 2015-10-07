45eefea03f6f62149adecc27ecaae8af40365b6e
Ricky Limka <ricky.limka@uqconnect.edu.au>
2015-10-08 01:58:39 +1000

    test get vid location #9

--- templates/video.php
+++ templates/video.php
@@ -43,7 +43,9 @@ $location
                     
                     <script>
                         function initialize() {
-                            var latLng = <?php echo "'".$location."'"; ?>;
+                            alert('nanannanana');
+                            
+                            /* var latLng = <?php echo "'".$location."'"; ?>;
                             latLng = latLng.split(" ");
                             
                             var myLatlng = new google.maps.LatLng( latLng[0], latLng[1] );
@@ -59,7 +61,7 @@ $location
                                 map: map,
                                 position: myLatlng,
                                 title: 'Hello World!'
-                            });
+                            }); */
                         }
                     </script>
                     <iframe src="map.html" width="480" height="320" seamless=""></iframe>
