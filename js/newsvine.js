fe344f70be6287d617927756bbf3bcda609db418
Ricky Limka <rikki@chikurin.example.org>
2015-10-08 14:18:20 +1000

    test comments #6

--- js/newsvine.js
+++ js/newsvine.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
     
     
     $('.nwsv-comment-box input').click(function() {
+        var comment-box = $(this).closest('form').find('textarea');
+        var comment-text = comment-box.text();
+        
         var comments = $(this).closest('form').prev();
-        comments.append("<li class='ui-li-static ui-body-inherit ui-last-child'>nanannananna</li>");
+        comments.append("<li class='ui-li-static ui-body-inherit ui-last-child'>" + comment-text + "</li>");
         comments.find(".ui-last-child").first().removeClass("ui-last-child");
+        
+        comment-box.text("");
     });
     
 });
