781eb24c72f7c3aedc6df3438437733f0336f9f1
Ricky Limka <rikki@chikurin.example.org>
2015-10-08 21:33:39 +1000

    added infowindow to google maps markers

--- map.php
+++ map.php
@@ -34,15 +34,18 @@
     
 	<style>
 		html {
+            width: 100%;
 			height: 100%;
 			overflow: hidden;
 		}
 		body {
 			margin: 0;
 			padding: 0;
+            width: 100%;
 			height: 100%;
 		}
 		#map_canvas {
+            width: 100%;
 			height: 100%;
 		}
 	</style>
