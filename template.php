83bdea94c390cde0704de105d3419343bbf3e8fd
Ricky Limka <rikki@chikurin.example.org>
2015-10-08 20:49:12 +1000

    added infowindow to google maps markers

--- template.php
+++ template.php
@@ -175,6 +175,16 @@
                     title: "your pos!",
                     icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
                 });
+                
+                var contentString = "<h1>Your Position<h1>"
+                
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                });
             }
         });
     </script>
